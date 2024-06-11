77f93f6d0aa99e345103af41c5a5e17f215cce13
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-06-11 19:43:40 +0300

    router hooks with client component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,25 @@
+"use client";
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
+
 const PropertyPage = () => {
-  return <div>Property Page</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { id } = useParams();
+  const name = searchParams.get("name");
+  const pathname = usePathname();
+
+  return (
+    <div>
+      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
+        Go Home {id} {name} {pathname}
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
