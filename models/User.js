077f111df05868e58e29ac631777069324832090
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-06-12 04:11:10 +0300

    Fetching data from server component

---
+++ models/User.js
@@ -0,0 +1,31 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email already exists"],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Property",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
